6f2c8cf5d25c3d2bbd1354dfc7ab5f8721a1878f
AutiCodes <kelvin@aerobytes.nl>
2025-01-24 23:25:20 +0100

    Fixed Github misalignment

--- index.php
+++ index.php
@@ -219,28 +219,23 @@
 
         <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
           <h3 class="resume-title">Projects</h3>
-
           <div class="resume-item pb-0">
             <h4>Kiyomi</h4>
             <p>
               <em>
-                An custom solution build for <a _blank href="https://trmc.nl target="">TRMC</a>. It
+                A custom solution built for <a _blank href="https://trmc.nl" target="">TRMC</a>. It
                 allows for club members to register their flights easily before taking flight on the
-                club field. Management from TRMC can then view the flights, export them for them for the
-                local authority and more.
+                club field. Management from TRMC can then view the flights, export them for the local authority and more.
               </em>
             </p>
             <ul>
-              <li><a href="">
+              <li>
                 <div class="social-links">
-                  <a class="text-white" href="https://github.com/AeroBytesNL/Kyomi-V2"
-                     target="_blank">
-                    <i class="bi bi-github">
-                      Github
-                    </i>
+                  <a class="text-white" href="https://github.com/AeroBytesNL/Kyomi-V2" target="_blank">
+                    <i class="bi bi-github"></i> Github
                   </a>
                 </div>
-              </a></li>
+              </li>
               <li>In active development</li>
             </ul>
           </div>
@@ -249,16 +244,13 @@
             <h4>CheckStack</h4>
             <p><em>An uptime monitor, because the existing one's didn't fit my need</em></p>
             <ul>
-              <li><a href="">
+              <li>
                 <div class="social-links">
-                  <a class="text-white" href="https://github.com/AeroBytesNL/CheckStack"
-                     target="_blank">
-                    <i class="bi bi-github">
-                      Github
-                    </i>
+                  <a class="text-white" href="https://github.com/AeroBytesNL/CheckStack" target="_blank">
+                    <i class="bi bi-github"></i> Github
                   </a>
                 </div>
-              </a></li>
+              </li>
               <li>In active development</li>
             </ul>
           </div>
@@ -267,15 +259,13 @@
             <h4>Contributor Invoiceplane</h4>
             <p><em>An Invoice management system. Fixing bugs for V1. Contributor V2.</em></p>
             <ul>
-              <li><a href="">
+              <li>
                 <div class="social-links">
                   <a class="text-white" href="https://github.com/InvoicePlane" target="_blank">
-                    <i class="bi bi-github">
-                      Github
-                    </i>
+                    <i class="bi bi-github"></i> Github
                   </a>
                 </div>
-              </a></li>
+              </li>
               <li>Still ongoing</li>
             </ul>
           </div>
@@ -286,63 +276,45 @@
 
           <div class="resume-item pb-0">
             <h4>Wordpress plugin</h4>
-            <p><em>An Wordpress plugin to send an notification to an Discord channel when a new article has
-              been posted</em></p>
+            <p><em>A Wordpress plugin to send a notification to a Discord channel when a new article has been posted</em></p>
             <ul>
-              <li><a href="">
+              <li>
                 <div class="social-links">
-                  <a class="text-white"
-                     href="https://github.com/AeroBytesNl/Wordpress-posts-to-Discord" target="_blank">
-                    <i class="bi bi-github">
-                      Github
-                    </i>
+                  <a class="text-white" href="https://github.com/AeroBytesNl/Wordpress-posts-to-Discord" target="_blank">
+                    <i class="bi bi-github"></i> Github
                   </a>
                 </div>
-              </a></li>
+              </li>
               <li>Finished</li>
             </ul>
           </div>
-
+          
           <div class="resume-item pb-0">
             <h4>Get my IP</h4>
-            <p>
-              <em>
-                I was done with pulling 10MB+ to get my WAN IP. So i've made my own!
-              </em>
-            </p>
+            <p><em>I was done with pulling 10MB+ to get my WAN IP. So I've made my own!</em></p>
             <ul>
-              <li><a href="">
+              <li>
                 <div class="social-links">
-                  <a class="text-white" href="https://github.com/AeroBytesNl/get-my-ip"
-                     target="_blank">
-                    <i class="bi bi-github">
-                      Github
-                    </i>
+                  <a class="text-white" href="https://github.com/AeroBytesNl/get-my-ip" target="_blank">
+                    <i class="bi bi-github"></i> Github
                   </a>
                 </div>
-              </a></li>
+              </li>
               <li>Finished</li>
             </ul>
           </div>
 
           <div class="resume-item pb-0">
             <h4>AllDayBot</h4>
-            <p>
-              <em>
-                A Discord bot build for a tech&gaming community
-              </em>
-            </p>
+            <p><em>A Discord bot built for a tech & gaming community</em></p>
             <ul>
-              <li><a href="">
+              <li>
                 <div class="social-links">
-                  <a class="text-white" href="https://github.com/AeroBytesNL/AllDayBot"
-                     target="_blank">
-                    <i class="bi bi-github">
-                      Github
-                    </i>
+                  <a class="text-white" href="https://github.com/AeroBytesNL/AllDayBot" target="_blank">
+                    <i class="bi bi-github"></i> Github
                   </a>
                 </div>
-              </a></li>
+              </li>
               <li>In passive development</li>
             </ul>
           </div>
